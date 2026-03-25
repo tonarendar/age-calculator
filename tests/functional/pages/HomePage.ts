bcc97ffed255d6731e58b9a18d6ab1330e245cdc
tonarendar <tonarendar@gmail.com>
2026-03-25 13:09:35 +0000

    doc: clean up comments

--- tests/functional/pages/HomePage.ts
+++ tests/functional/pages/HomePage.ts
@@ -1,59 +1,53 @@
-import { Locator, Page } from '@playwright/test';
+import { Locator, Page } from "@playwright/test";
 
 export class HomePage {
+  //1.Locators:
+  private readonly page: Page;
+  private readonly pageHeader: Locator;
+  private readonly dateInput: Locator;
+  private readonly submitButton: Locator;
+  private readonly clearButton: Locator;
+  private readonly resultText: Locator;
 
-    //1.Identify variables - private and readonly
-    // using encapsulation - wrapping of data into variabels that is only accessile inside the class
+  //2.Constructor: setup method that runs automatically when new Page Object is created.
+  constructor(page: Page) {
+    this.page = page;
+    this.pageHeader = page.getByRole("heading", {
+      name: "Birthdate Age Calculator",
+    });
+    this.dateInput = page.getByRole("textbox", { name: "Date of Birth" });
+    this.submitButton = page.getByRole("button", { name: "Submit" });
+    this.clearButton = page.getByRole("button", { name: "Clear" });
+    this.resultText = page.getByTestId("result");
+  }
 
-    private readonly page: Page;
-    private readonly pageHeader: Locator;
-    private readonly dateInput: Locator;
-    private readonly submitButton: Locator;
-    private readonly clearButton: Locator;
-    private readonly resultText: Locator;
+  //3. Action methods: methods that perform actions on the page.
 
+  async visit() {
+    await this.page.goto("");
+  }
 
-    //2.Constructor
-    //The constructor is the setup method that runs automatically when you create a new Page Object .
-    // its job is to receive the Playwright page, store it, and define all the locators.
-    //  so every method in the class can use them."
+  async fillDateOfBirth(dateOfBirth: string) {
+    await this.dateInput.fill(dateOfBirth);
+  }
 
-    constructor(page: Page) {
-        this.page = page;
-        this.pageHeader = page.getByRole('heading', { name: 'Birthdate Age Calculator' });
-        this.dateInput = page.getByRole('textbox', { name: 'Date of Birth' });
-        this.submitButton = page.getByRole('button', { name: 'Submit' });
-        this.clearButton = page.getByRole('button', { name: 'Clear' });
-        this.resultText = page.getByTestId('result');
-    }
+  async clickSubmit() {
+    await this.submitButton.click();
+  }
 
-    //3. Action methods
+  async clickClear() {
+    await this.clearButton.click();
+  }
 
-    async visit() {
-        await this.page.goto('');
-    }
+  getPageHeader(): Locator {
+    return this.pageHeader;
+  }
 
-    async fillDateOfBirth(dateOfBirth: string) {
-        await this.dateInput.fill(dateOfBirth);
-    }
+  getDateInput(): Locator {
+    return this.dateInput;
+  }
 
-    async clickSubmit() {
-        await this.submitButton.click();
-    }
-
-    async clickClear() {
-        await this.clearButton.click();
-    }
-
-    getPageHeader(): Locator {
-        return this.pageHeader;
-    }
-
-    getDateInput(): Locator {
-        return this.dateInput;
-    }
-
-    getResultText(): Locator {
-        return this.resultText;
-    }
+  getResultText(): Locator {
+    return this.resultText;
+  }
 }
